877fd4d867d00fd1cd08af1219b38aa5abfe2efc
onadegibert <onadegibert@gmail.com>
2021-11-02 16:56:58 +0100

    sample 3 sents per article

--- human_evaluation_tsv.py
+++ human_evaluation_tsv.py
@@ -48,41 +48,45 @@ def normalize(sentence, lang):
     return sentence
 
 
-def get_random_sample(text, lang):
+def get_random_sample(text, lang, samples_per_article):
     sentences = split_text_into_sentences(text=text, language=lang)
-    # Deduplicate sentences
-    sentences_dedup = list(set(sentences))
     # Remove sentences that don't contain letters
-    sentences_clean = [sentence for sentence in sentences_dedup if re.match(r'[a-zA-Z]{2,}',sentence)]
-    # Filter sentences that are smaller than three words
-    sentences_filtered = [sentence for sentence in sentences_clean if len(sentence.split())>2]
+    sentences_clean = [sentence for sentence in sentences if re.match(r'[a-zA-Z]{2,}',sentence)]
+    # Filter sentences that are smaller than ten words and smaller than 40
+    sentences_filtered = [sentence for sentence in sentences_clean if len(sentence.split())>=10 and len(sentence.split())<=40]
     # Normalize punctuation
     sentences_norm = [normalize(sentence, lang) for sentence in sentences_filtered]
-
-    sentences_sample = random.choices(sentences_norm, k=60)
-    return sentences_sample
+    # Random sample 3 sentences
+    try:
+        sentence_sample = random.choices(sentences_norm, k=3)
+    except:
+        sentence_sample = ['']
+    return sentence_sample
 
 if __name__ == '__main__':
     lang = sys.argv[1]
     PATH = os.path.join('data','human_eval',lang+'.json')
-    models = {'human': '', 'ada':'', 'babbage':'', 'curie':'', 'davinci':''}
+    models = {'human': [], 'ada': [], 'babbage': [], 'curie': [], 'davinci': []}
     with open(PATH, 'r') as f:
-        for element in f:
-            element = json.loads(element)
-            element["human"] = element.pop("text")
+        for article in f:
+            article = json.loads(article)
+            article["human"] = article.pop("text")
             for model in models:
-                models[model] = '. '.join([models[model],element[model]])
-    
+                models[model].append(article[model])
+
     human_eval = pd.DataFrame()
     for model in models:
-        sample = get_random_sample(models[model],lang)
-        sample_model = list(zip(sample, [model]*60))
+        samples_per_article = []
+        for article in models[model]:
+            sentence_sample = get_random_sample(article, lang, samples_per_article)
+            samples_per_article.extend(sentence_sample)
+        sample_model = list(zip(samples_per_article, [model]*60))
         human_eval = human_eval.append(sample_model, ignore_index=True)
     human_eval = human_eval.rename(columns={0:'sentence',1:'model'})
+
     # random
     #human_eval_random = human_eval.sample(frac=1)
     #human_eval_random.to_csv(human_eval_tsv,sep='\t')
 
-    human_eval_tsv = os.path.join('data','human_eval',lang+'.tsv')
+    human_eval_tsv = os.path.join('data','human_eval',lang+'_test.tsv')
     human_eval.to_csv(human_eval_tsv,sep='\t')
-
